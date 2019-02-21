61a430b9bb6695e584e624671ce7000450194bca
OlgaKolosova <47844368+OlgaKolosova@users.noreply.github.com>
2019-02-21 10:57:51 +0300

    Update Expr.ml

--- src/Expr.ml
+++ src/Expr.ml
@@ -43,8 +43,9 @@ let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
 *)
-let int2bool i=i!=0;;
-let bool2int b=if b then 1 else 0;;
+let int2bool i=i !=0
+let bool2int b=if b then 1 else 0
+
 let operator op_operator left right = match op_operator with
 |"+" -> left + right
 |"-" -> left - right
@@ -70,6 +71,6 @@ let operator op_operator left right = match op_operator with
 (* let eval = failwith "Not implemented yet" *)
                     
 let rec eval state expr = match expr with
-|Const const -> const
+|Const c -> c
 |Var vr -> state vr
 |Binop(op_operator,left,right) -> (operator op_operator) (eval state left) (eval state right);;
